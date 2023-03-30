4ecb2c46d023aab00415cce912965fcd6af9b91e
Rand al'Thor <gbenincasa042@gmail.com>
2023-03-29 23:02:37 -0300

    a2

---
+++ src/ejercicios/Ejercicio2Guia.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios;
+
+/**
+ *
+ * @author gbeni
+ */
+public class Ejercicio2Guia {
+    
+    public static void main(String[] args){
+    
+        double a,b;
+        
+        a = 12;
+        b = a/22 + 3;
+        
+        System.out.println("el valor de a es " + a + " y el valor de b es " + b);
+    
+    }
+    
+}
